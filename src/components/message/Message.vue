f48eacecd5349654ff65c237b96d3fc82d9af151
Azamat <azamat@gmail.com>
2024-02-23 21:30:18 +0500

    added new tag and style on message component

--- src/components/message/Message.vue
+++ src/components/message/Message.vue
@@ -2,13 +2,14 @@
     <div class="message">
         <h3 class="message__title">Спасибо за покупку!</h3>
         <div v-for="item in purchasedProductsStore?.purchased" :key="item.id" class="message__card">
-            <h3 class="message__card-title">{{ item?.title }}</h3>
+            <h3 class="message__card-title">"{{ item?.title }}"</h3>
+            <p class="message__card-productAmount">количество: {{ item.amount }}</p>
             <p class="message__card-info">
-                {{ Math.floor((item?.price - (item?.price * item?.discountPercentage / 100))) }} X {{ item.amount }}
+               Цена: {{ Math.floor((item?.price - (item?.price * item?.discountPercentage / 100))) }}$ 
             </p>
         </div>
          <p class="message__totalPrice">
-           Итоговая цена: {{ totalPrice }}
+           Итоговая цена: {{ totalPrice }}$
          </p>
     </div>
 </template>
